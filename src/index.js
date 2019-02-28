3e2e53e25f6521cae95ddfe9cfd03f6e55582e92
Pavel Leskavets <leskovecpavel7@gmail.com>
2019-02-28 20:51:27 +0300

    my implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = [];
+   for(let i = 0; i < str.length; i++) {
+    for(let j = 0; j < bracketsConfig.length; j++) {
+      if(str.charAt(i) == bracketsConfig[j][0] && str.charAt(i) != bracketsConfig[j][1]) {
+        arr.push(str.charAt(i));
+        continue;
+      }
+      else if(str.charAt(i) == bracketsConfig[j][1] && arr[arr.length - 1] == bracketsConfig[j][0] && str.charAt(i) != bracketsConfig[j][0] ) {
+        arr.pop();
+        continue;
+      }
+      else if(str.charAt(i) == bracketsConfig[j][1] && arr[arr.length - 1] != bracketsConfig[j][0] && str.charAt(i) != bracketsConfig[j][0] ) {
+        return false
+      }
+      else if(str.charAt(i) == bracketsConfig[j][1] && str.charAt(i) == bracketsConfig[j][0] && arr.indexOf(str.charAt(i))  === -1 ) {
+          arr.push(str.charAt(i));
+          continue;    
+      }
+      else if(str.charAt(i) == bracketsConfig[j][1] && str.charAt(i) == bracketsConfig[j][0] && arr.indexOf(str.charAt(i))  >= 0 && arr[arr.length - 1] === str.charAt(i) ) {
+        arr.pop();
+        continue;
+      }
+      else  {
+         continue
+      }
+      
+    }
+  }
+  if(arr.length == 0 ) {
+    return true
+  }
+
+  return false
+  
 }
